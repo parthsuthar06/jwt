4f70588a3e6789118284ee8860c221a8d1f095e6
parthsuthar06 <psuthar003@gmail.com>
2019-06-10 15:16:10 +0530

    chore(indentation)

--- jwt.js
+++ jwt.js
@@ -9,7 +9,8 @@ const SECRET = 'mysecret'
 /**
  * Case 2: 'RS256' -> Required public private key pair
  * generated : https://qsandbox.com/tools/private-public-keygen
- * and format above key using linux cmd 'openssl rsa -in private_key_filename -pubout -outform PEM -out public_key_output_filename'
+ * and format above key using below linux cmd:
+ * 'openssl rsa -in private_key_filename -pubout -outform PEM -out public_key_output_filename'
  */
 const PUBLICKEY = fs.readFileSync('./public.key', 'utf8') //for other ms validating token
     , PRIVATEKEY = fs.readFileSync('./private.key', 'utf8'); // for AuthN server-side
